9ba6becb719e40e5ade23acd29396ea608c127c0
abdelaziz123456 <abdelazizmohamed1233@gmail.com>
2023-04-27 16:44:21 +0200

    small add

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -15,7 +15,56 @@ exports.notExisting = (res) => {
 //getAllTours handler
 exports.getToursHandler = async (req, res) => {
   try {
-    const allTours = await Tour.find();
+    let queryObject = { ...req.query };
+
+    let excludedFileds = ['limit', 'page', 'field', 'sort'];
+
+    excludedFileds.forEach((el) => delete queryObject[el]);
+
+    //{difficulty:'easy',duration:{$gte:5}}
+    //{ duration: { gte: '5' }, difficulty: 'easy' }
+    let queryStr = JSON.stringify(queryObject);
+
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //for sorting
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // for limiting fields
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v -description');
+    }
+
+    // for pagination
+
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numOfTours = await Tour.countDocuments();
+
+      if (skip >= numOfTours) {
+        throw new Error("this page doesn't exist");
+      }
+    }
+    const allTours = await query;
+    //const allTours = await Tour.find(req.query);
     res.status(200).json({
       status: 'success',
       results: allTours.length,
